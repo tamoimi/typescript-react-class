a7410c700c6b5a57525214fbb2480a45f2539729
tami <xorud7979@gmail.com>
2023-03-31 15:11:39 +0900

    feat: add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  color: ${(props) => props.theme.textColor};
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-    <Box>Hello world!</Box>
-  </Wrapper>
+    <div>
+      <Circle bgColor={"teal"}/>
+      <Circle bgColor={"tomato"}/>
+      typescript
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111"
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke"
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
@@ -30,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
